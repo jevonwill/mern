d123b320d48100ed42911ce6f036d23b75509258
jevon.williams <jevon.williams@madmobile.com>
2021-07-13 15:57:06 -0400

    added student_data.json to repo

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
-         const port = 3000;
-         http.createServer((req, res) => {
-             res.writeHead(200, {
-               "Content-Type": "text/plain"
-             });
-             res.write("Hello Readers!");
-             res.end();
-        }).listen(port);
-        console.log(`Node Server is running on port : ${port}`)
+const port = 3000;
+const studentsData = require(' ./student_data.json');
+http.createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    });
+    res.write(JSON.stringify(studentsData));
+    res.end();
+}).listen(port);
+console.log(`Node Server is running on port : ${port}`)
