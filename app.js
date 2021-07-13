0af801c2e83cd72aff2eba06f731b05f6965eab9
jevon.williams <jevon.williams@madmobile.com>
2021-07-13 15:44:48 -0400

    finally got nginx to work

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+const http = require('http');
+         const port = 3000;
+         http.createServer((req, res) => {
+             res.writeHead(200, {
+               "Content-Type": "text/plain"
+             });
+             res.write("Hello Readers!");
+             res.end();
+        }).listen(port);
+        console.log(`Node Server is running on port : ${port}`)
