89adc891ef903772180d5f323524c626e3449724
jevon.williams <jevon.williams@madmobile.com>
2021-08-05 10:01:03 -0400

    Updated app.js to include sendFile function

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 5000;
 
 app.get('/', (req, res) => {
-	res.send('I hope this works!')
+	res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(port, () => {
 
 })
 
-app.use(express.static('app'))
 
